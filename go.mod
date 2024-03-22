ecf27e41783294d80145610ed1d89bff31fab17c
boonsiangteh <tehboonsiang@gmail.com>
2024-03-23 00:30:25 +0800

    got access token from github

--- go.mod
+++ go.mod
@@ -17,6 +17,8 @@ require (
 	github.com/go-playground/universal-translator v0.18.1 // indirect
 	github.com/go-playground/validator/v10 v10.19.0 // indirect
 	github.com/goccy/go-json v0.10.2 // indirect
+	github.com/golang/protobuf v1.5.3 // indirect
+	github.com/google/go-cmp v0.5.9 // indirect
 	github.com/json-iterator/go v1.1.12 // indirect
 	github.com/klauspost/cpuid/v2 v2.2.7 // indirect
 	github.com/leodido/go-urn v1.4.0 // indirect
@@ -29,8 +31,10 @@ require (
 	golang.org/x/arch v0.7.0 // indirect
 	golang.org/x/crypto v0.21.0 // indirect
 	golang.org/x/net v0.22.0 // indirect
+	golang.org/x/oauth2 v0.18.0 // indirect
 	golang.org/x/sys v0.18.0 // indirect
 	golang.org/x/text v0.14.0 // indirect
+	google.golang.org/appengine v1.6.7 // indirect
 	google.golang.org/protobuf v1.33.0 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
--- internal/server/routes.go
+++ internal/server/routes.go
@@ -3,13 +3,37 @@ package server
 import (
 	"net/http"
 
+	"fmt"
+	"log"
+	"os"
+
 	"github.com/gin-gonic/gin"
+	"golang.org/x/oauth2"
+	"golang.org/x/oauth2/github"
 )
 
-func (s *Server) RegisterRoutes() http.Handler {
-	r := gin.Default()
+var conf = &oauth2.Config{
+	ClientID:     "960d9cbc4263c2d7102a",
+	ClientSecret: "73503be1be93606171c6f63d81b2b7bb0ba72490",
+	Endpoint:     github.Endpoint,
+	RedirectURL:  "http://localhost:8080/oauth/amazon/receive",
+}
 
+var state = "somesteadystate"
+var pkce_verifier = oauth2.GenerateVerifier()
+
+func (s *Server) RegisterRoutes() http.Handler {
+
+	r := gin.Default()
+	path, err := os.Getwd()
+	if err != nil {
+		log.Println(err)
+	}
+	fmt.Println("path: ", path)
+	r.LoadHTMLGlob("internal/templates/*")
 	r.GET("/", s.HelloWorldHandler)
+	r.POST("/oauth/amazon/login", s.oauthAmazonHandler)
+	r.GET("/oauth/amazon/receive", s.oauthAmazonReceiver)
 
 	return r
 }
@@ -18,5 +42,38 @@ func (s *Server) HelloWorldHandler(c *gin.Context) {
 	resp := make(map[string]string)
 	resp["message"] = "Hello World"
 
-	c.JSON(http.StatusOK, resp)
+	// c.JSON(http.StatusOK, resp)
+	c.HTML(http.StatusOK, "index.html", nil)
+}
+
+func (s *Server) oauthAmazonHandler(c *gin.Context) {
+	// resp := make(map[string]string)
+	// resp["message"] = "Hello World"
+
+	// pkce verifier
+	url := conf.AuthCodeURL(state, oauth2.S256ChallengeOption(pkce_verifier))
+	fmt.Println("Auth Url: " + url)
+
+	c.Redirect(http.StatusSeeOther, url)
+
+}
+
+func (s *Server) oauthAmazonReceiver(c *gin.Context) {
+	resp := make(map[string]string)
+	code := c.Query("code")
+	state := c.Query("state")
+
+	fmt.Println("code: " + code)
+	fmt.Println("state: " + state)
+	resp["code"] = code
+	resp["state"] = state
+	// c.JSON(http.StatusOK, resp)
+
+	token, err := conf.Exchange(c.Request.Context(), code, oauth2.VerifierOption(pkce_verifier))
+	if err != nil {
+		fmt.Println("error exchanging token: ", err)
+	}
+
+	fmt.Println("%+v", token)
+	c.JSON(200, token)
 }
